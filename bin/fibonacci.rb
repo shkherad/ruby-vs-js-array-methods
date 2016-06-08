08d4122d1a764b43e7b5bc81d50b1d7703fe769b
shkherad <shkherad@bu.edu>
2016-06-08 16:35:25 -0400

    class demo notes added
    pulled from demo branch

--- bin/fibonacci.rb
+++ bin/fibonacci.rb
@@ -1,72 +1,24 @@
 fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-
-twice_fibs = fibs.map { |fib| fib*2}
-
 puts "fibs is #{fibs}"
-#if have a longer block
 
-# fibs.map do |fib|
-#   fib * 2
-# end
+# twice_fibs = fibs.map { |fib| fib * 2 }
+# puts "twice_fibs is #{twice_fibs}"
 
-#.map in ruby returns an array
+# less_10 = fibs.map { |fib| fib * 2 }.select { |fib| fib < 10 }
+# puts "less_10 is #{less_10}"
 
+sum_fibs = fibs.reduce(:+)
+puts "sum_fibs is #{sum_fibs}"
 
-# filter version of ruby is selsce
+# fibs_from_one = fibs[1..-1]
+fibs_from_one = fibs.reject { |fib| fib == 0 }
+puts "fibs_from_one is #{fibs_from_one}"
 
-#defining a function
-def times_two(number)
-  number * 2
-end
+product_fibs = fibs_from_one.reduce(:*)
+puts "product_fibs is #{product_fibs}"
 
-fibs_filtered = fibs.map { |fib| fib*2}.select { |fib| fib < 10}
+sum_fibs_odd = fibs.select(&:odd?).reduce(:+)
+puts "sum_fibs_odd is #{sum_fibs_odd}"
 
-#if not fit on one line
-
-filtered = fibs.map do |fib|
-  fib * 2
-end.select do |fib|
-  fib < 10
-end
-
-
-
-puts "twice fibs is #{twice_fibs}"
-puts "fibs filtered is #{fibs_filtered}"
-
-puts "filtered is #{filtered}"
-
-
-
-#sum
-
-sum=0
-fibs.each { |fib| sum+= fib}
-puts "The sum of the fibs is  #{sum}"
-
-#odd elements
-
-odd_fibs = fibs.select do |fib|
-  fib.odd?
-end
-
-puts "The odd fibs are #{odd_fibs}"
-
-
-odd_sum=0
-#sum of the odd fibs
-odd_fibs = fibs.select do |fib|
-  fib.odd?
-end.each { |fib| odd_sum+= fib}
-
-puts  "The sum of the odd fibs are #{odd_sum}"
-
-#even fibs product
-
-
-even_product = 0
-fib_evens = fibs.select do |fib|
-  fib.even?
-end.drop(1).each { |fib| even_product*= fib}
-
-puts "The even fibs are #{fib_evens}"
+product_fibs_even = fibs_from_one.select(&:even?).reduce(:*)
+puts "product_fibs_even is #{product_fibs_even}"
